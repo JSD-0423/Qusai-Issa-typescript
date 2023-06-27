7421e967f3946e4f267fee0f4b2561a03a7e76ee
qusai122 <qusai.2010.123@gmail.com>
2023-06-27 15:43:28 +0300

    test

---
+++ utils/tokens.ts
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (payload: any, secret: string) => {
+	const token = jwt.sign(payload, secret);
+	return token;
+}
+
+const decodeToken = (token: string, secret: string) => {
+	const decoded = jwt.verify(token, secret);
+	return decoded;
+}
